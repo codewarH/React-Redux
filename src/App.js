1c8ba1867419c163219983312bf45512cae10f6b
codewarH <44767714+codewarH@users.noreply.github.com>
2020-08-01 01:19:32 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions/'
+
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const logged = useSelector((state) => state.logged)
+  const dispatch = useDispatch()
+
+  return (
+    <div className="App">
+      <h1>Counter {counter} </h1>
+      <button onClick={() => dispatch(increment(5))}> + </button>
+      <button onClick={() => dispatch(decrement())}> - </button>
+
+      {logged ? <h3> Valuable information I shouldn't see </h3> : ''}
+    </div>
+  )
+}
+
+export default App
